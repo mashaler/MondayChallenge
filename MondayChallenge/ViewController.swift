1c5309a1380e22a29295108ec71526624a4412ce
mashaler <mashalerolva@gmail.com>
2023-05-22 08:08:54 +0200

    Initial Commit

---
+++ MondayChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MondayChallenge
+//
+//  Created by DA MAC  M1 154 on 2023/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
